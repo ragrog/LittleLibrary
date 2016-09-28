125a75ad0cfebc03fd7e76dd6f497d89868f9ba0
ragrog <ragrog216@outlook.jp>
2016-09-28 10:20:07 +0900

    購入申請のモデルとコントローラを作成

---
+++ app/Controller/PurchaseRequestController.php
@@ -0,0 +1,18 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class PurchaseRequestController extends AppController {
+	public function index()
+	{
+
+	}
+	public function edit($id = null){
+
+	}
+
+	public function add(){
+
+
+	}
+}
---
+++ app/Model/PurchaseRequest.php
@@ -0,0 +1,68 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class PurchaseRequest extends AppModel {
+	public $useTable = 'ppurchase_requests';
+	// public $validate = array(
+	
+	// );
+	// public $fieldList = array(
+	// 	'evaluation',
+	// 	'sentence'
+	// );
+	// public function edit($userId, $bookId, $request) {
+	// 	// もしもレビューが存在したら、idをsaveする
+	// 	$id = $this->getReviewId($userId, $bookId);
+	// 	if ($id  !== false) {
+	// 		$saveData['id'] = $id;
+	// 	}
+	// 	// フィールドリストにあるカラムだけ保存
+	// 	foreach ($this->fieldList as $value) {
+	// 		$saveData[$value] = isset($request['Review'][$value]) ? $request['Review'][$value] : '';
+	// 	}
+
+	// 	$saveData['user_id'] = $userId;
+	// 	$saveData['book_info_id'] = $bookId;
+
+	// 	if ($this->save($saveData)) {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	
+	// }
+	// public function getReviewId($userId, $bookId) {
+	// 	$data = $this->getReview($userId, $bookId);
+	// 	if (empty($data)) {
+	// 		return false;
+	// 	} else {
+	// 		return $data['Review']['id'];
+	// 	}
+	// }
+	// public function getReview($userId, $bookId) {
+	// 	return $this->find('first', array(
+	// 		'conditions' => array(
+	// 			'Review.user_id' => $userId,
+	// 			'Review.book_info_id' => $bookId
+	// 		)
+	// 	));
+	// }
+	// public function getReviewAll($options = []) {
+	// 	$options['order'][] = 'Review.modified';
+	// 	return $this->find('all', $options);
+	// }
+	// public function getReviewByUserId($userId, $options = []) {
+	// 	$options['conditions']['Review.user_id'] = $userId;
+	// 	return $this->find('all', $options);
+	// }
+	// public function getReveiwByBookId($bookId, $options = []) {
+	// 	$options['conditions']['Review.book_id'] = $BookId;
+	// 	return $this->find('all', $options);
+	// }
+	// public function deleteReview($userId, $bookId) {
+	// 	if (($id = $this->getReviewId($userId, $bookId)) === false) {
+	// 		return false;
+	// 	}
+	// 	return $this->delete($id);
+	// }
+}
